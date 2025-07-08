21ddb96cedeff0aed3f6c2c759bb06664739724c
carlos lopez <carloslopez@clformacion.es>
2025-07-08 17:30:31 +0200

    creacion de app

--- programacion/js/js.js
+++ programacion/js/js.js
@@ -7,3 +7,30 @@ imc=peso/(altura*altura);
 document.getElementById('demo').innerHTML="Su IMC es "+ imc.toFixed(2);
 
 }
+
+function regalo(){
+const costo=50;
+let inscripcion;
+let precio;
+let descuento;
+let oferta;
+let nombre;
+let apellido;
+
+inscripcion=prompt("Numero de participantes que desea inscribir");
+nombre=prompt("tu nombre");
+apellido=prompt("tu apellido");
+precio=inscripcion*costo;
+descuento=precio*0.10;
+oferta=precio-descuento;
+document.getElementById('demo').innerHTML=inscripcion;
+document.getElementById('demo2').innerHTML=costo;
+document.getElementById('demo3').innerHTML=precio;
+document.getElementById('demo4').innerHTML=descuento;
+document.getElementById('demo5').innerHTML=oferta;
+document.getElementById('demo6').innerHTML=nombre;
+document.getElementById('demo7').innerHTML=apellido;
+
+
+
+}
